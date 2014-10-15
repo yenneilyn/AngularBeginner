a334b4fa065e54cfadef942b42befb255632b5b1
yenneilyn <yenneilyn@hotmail.com>
2014-10-14 22:42:28 -0400

    added play audio function

--- app/app.js
+++ app/app.js
@@ -1,12 +1,23 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var app = angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+ // 'myApp.view1',
+ // 'myApp.view2',
+ // 'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+
+app.controller('PlayerController', ['$scope', function($scope) {
+   $scope.playing = false;
+    $scope.audio = document.createElement('audio');
+    $scope.audio.src = '/Downloads/npr.mp4';
+
+}]);
+
+app.controller('RelatedController', ['$scope', function($scope) {
+
 }]);
